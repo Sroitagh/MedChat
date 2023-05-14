b96210eb618a54ea16507d4f6e78c1dfc828e222
SroitaPC <sroita18@gmail.com>
2023-05-14 21:53:37 +0530

    components update

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,6 @@
+import TeamChannelList from './TeamChannelList';
+
 export  {default as ChannelContainer } from './ChannelContainer';
 export  {default as ChannelListContainer } from './ChannelListContainer';
 export  { default as ChannelSearch } from './ChannelSearch';
+export  { default as TeamChannelList } from './TeamChannelList';
