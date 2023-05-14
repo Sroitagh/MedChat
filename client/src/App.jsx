64f3c56f961536aa011c9d1c50c237f9fa9e92b6
SroitaPC <sroita18@gmail.com>
2023-05-14 13:59:55 +0530

    server running

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+import ChannelListContainer from './components/ChannelListContainer';
+import ChannelContainer from './components/ChannelContainer';
+
+import { ChannelListContainer, ChannelContainer } from './components';
+
+
+
+const apiKey = 'mzah3eb53htc'
+const client = StreamChat.getInstance(apiKey);
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='app__wrapper'>
+      <Chat client={client} theme="team light">
+                <ChannelListContainer 
+                    
+                />
+                <ChannelContainer 
+                   
+                />
+            </Chat>
     </div>
   )
 }
